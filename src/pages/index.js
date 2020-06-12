ff2267b0320b0f3b992ba456369c75d2cc89359c
Anneli <annelikripsaar@gmail.com>
2020-06-12 17:10:55 +0300

    add new banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div
       style={{
-        backgroundColor: "#ff8700",
+        backgroundColor: "#FE5000",
         height: "100vh",
         display: "flex",
         alignItems: "center",
@@ -18,7 +18,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Work Walks</title>
       </Helmet>
-      <img style={{ width: "50vw" }} src="images/esitlused-2020.gif"></img>
+      <img style={{ width: "70vw" }} src="images/esitlused-2020.gif"></img>
     </div>
   )
 }
