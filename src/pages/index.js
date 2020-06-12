bcbafdb6d58b2662f8435f1dd21ee505f99e257e
Anneli <annelikripsaar@gmail.com>
2020-06-12 13:37:51 +0300

    add website placeholder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import "../style.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div
+      style={{
+        backgroundColor: "#ff8700",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img style={{ width: "50vw" }} src="images/esitlused-2020.gif"></img>
+    </div>
+  )
 }
